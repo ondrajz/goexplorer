09ca5b3fa76e5abfdab1b325fdeae3525a3b38f0
TrueFurby <truefurby@gmail.com>
2017-01-26 02:57:18 +0100

    deps wip

--- node.go
+++ node.go
@@ -24,16 +24,24 @@ const (
 	Package           = "package"
 	Program           = "program"
 	Object            = "object"
+	Func              = "func"
 )
 
 type node struct {
 	Type NodeType
-	Loc  string
-	Dir  string
+	Loc  string `json:",omitempty"`
+	Dir  string `json:",omitempty"`
 
-	Id    string
-	Label string
-	Value int64
+	Id    string `json:"Id"`
+	Label string `json:"Label"`
+	Value int64  `json:"Value,omitempty"`
+
+	//Nodes []*node `json:"nodes,omitempty"`
+}
+
+type edge struct {
+	From string
+	To   string
 }
 
 func gopathHandler(w http.ResponseWriter, r *http.Request) {
@@ -117,6 +125,77 @@ func gopathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	b, err := json.MarshalIndent(nodes, "", "  ")
+	if err != nil {
+		logf("MarshalIndent failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/json")
+	w.Write(b)
+}
+
+func depsHandler(w http.ResponseWriter, r *http.Request) {
+	gopath := os.Getenv("GOPATH")
+	dirSrc := filepath.Join(gopath, "src")
+	impPath := "github.com/TrueFurby/go-callvis"
+
+	logf("deps for: %s", impPath)
+
+	pathMap := make(map[string]bool)
+	var addPkg func(path string)
+
+	/*impPkg, err := build.Import(impPath, dirSrc, 0)
+	if err != nil {
+		logf("build.Import %s failed: %v", path, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}*/
+	nodes := []*node{}
+	edges := []*edge{}
+
+	addPkg = func(path string) {
+		pkg, err := build.Import(path, dirSrc, 0)
+		if err != nil {
+			logf("build.Import %s failed: %v", path, err)
+			return
+		} else if pkg.Goroot {
+			return
+		}
+		if _, ok := pathMap[path]; !ok {
+			logf("addPkg %s", path)
+			pathMap[path] = true
+			var typ NodeType = Package
+			if pkg.IsCommand() {
+				typ = Program
+			}
+			nodes = append(nodes, &node{
+				Id:    pkg.ImportPath,
+				Label: pkg.Name,
+				Type:  typ,
+				//Nodes: nodes,
+			})
+		}
+		for _, p := range pkg.Imports {
+			addPkg(p)
+			edges = append(edges, &edge{
+				From: pkg.ImportPath, To: p,
+			})
+		}
+	}
+	addPkg(impPath)
+	/*&node{
+			Id:    pkg.ImportPath,
+			Label: pkg.Name,
+			Type:  Package,
+			Nodes: addPkg(impPath)
+	    }*/
+	var data = struct {
+		Nodes []*node
+		Edges []*edge
+	}{nodes, edges}
+
+	b, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
 		logf("MarshalIndent failed: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
